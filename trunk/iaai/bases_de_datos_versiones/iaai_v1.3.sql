cbf3a09f39c09e84f902a179ceb439ea26c88585
matiasmilesi <matiasmilesi@f1b130d6-d504-11de-bff8-d51c198f79d0>
2009-11-27 22:51:13 +0000

    Corregimos el tipo de fecha de naciemiento a Date en alumno

---
+++ trunk/iaai/bases_de_datos_versiones/iaai_v1.3.sql
@@ -0,0 +1,113 @@
+-- MySQL Administrator dump 1.4
+--
+-- ------------------------------------------------------
+-- Server version	5.1.36-community-log
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+--
+-- Create schema iaai
+--
+
+CREATE DATABASE IF NOT EXISTS iaai;
+USE iaai;
+
+--
+-- Definition of table `alumno`
+--
+
+DROP TABLE IF EXISTS `alumno`;
+CREATE TABLE `alumno` (
+  `id_alumno` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `dni` int(8) unsigned NOT NULL,
+  `fecha_nac` date NOT NULL,
+  `telefono_carac` int(5) unsigned DEFAULT NULL,
+  `telefono_numero` int(9) unsigned NOT NULL,
+  `escuela_nombre` varchar(100) DEFAULT NULL,
+  `escuela_año` int(1) unsigned DEFAULT NULL,
+  `direccion` varchar(100) NOT NULL,
+  `id_responsable` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id_alumno`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `alumno`
+--
+
+/*!40000 ALTER TABLE `alumno` DISABLE KEYS */;
+INSERT INTO `alumno` VALUES  (3,'Nico','Nicolas',30501831,'0000-00-00',0,2222,'',0,'asdf 123',0),
+ (4,'Matías','Milesi',31065760,'1984-06-28',0,4550871,'',0,'Pedro Ferré 1143',0);
+/*!40000 ALTER TABLE `alumno` ENABLE KEYS */;
+
+
+--
+-- Definition of table `profesor`
+--
+
+DROP TABLE IF EXISTS `profesor`;
+CREATE TABLE `profesor` (
+  `id_profesor` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `dni` int(8) unsigned NOT NULL,
+  `telefono_carac` int(5) unsigned NOT NULL,
+  `telefono_numero` int(9) unsigned NOT NULL,
+  `fecha_nac` date NOT NULL DEFAULT '2000-01-01',
+  `direccion` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_profesor`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `profesor`
+--
+
+/*!40000 ALTER TABLE `profesor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `profesor` ENABLE KEYS */;
+
+
+--
+-- Definition of table `responsable`
+--
+
+DROP TABLE IF EXISTS `responsable`;
+CREATE TABLE `responsable` (
+  `id_responsable` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre_respon` varchar(50) NOT NULL,
+  `apellido_respon` varchar(50) NOT NULL,
+  `fecha_nac` date NOT NULL,
+  `direccion` varchar(100) NOT NULL,
+  `telefono_carac` int(5) unsigned DEFAULT NULL,
+  `telefono_numero` int(9) unsigned NOT NULL,
+  `dni` int(8) unsigned NOT NULL,
+  PRIMARY KEY (`id_responsable`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `responsable`
+--
+
+/*!40000 ALTER TABLE `responsable` DISABLE KEYS */;
+INSERT INTO `responsable` VALUES  (1,'Nicolas','Mizerniuk','1111-11-11','aaaaaa 1111',0,111111,30501831);
+/*!40000 ALTER TABLE `responsable` ENABLE KEYS */;
+
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
