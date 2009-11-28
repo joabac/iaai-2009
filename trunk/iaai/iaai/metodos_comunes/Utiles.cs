ec7511361c1dbf3ed032e9438def1f84b23e0e42
joabac <joabac@f1b130d6-d504-11de-bff8-d51c198f79d0>
2009-11-28 17:37:13 +0000

    agregue clase para metodos comunes
    luego agrego comentarios pa`ra corregir y si llego armo un estandar de prog
    
    los metodos de base de datos modificarlos con try y catch

---
+++ trunk/iaai/iaai/metodos_comunes/Utiles.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iaai.metodos_comunes
+{
+    class Utiles
+    {
+
+        public bool ValidarDni(string dni)
+        {
+
+            if (dni.Length < 8)
+                return false;
+
+            foreach (char c in dni) 
+            {
+                if (!char.IsDigit(c))
+                    return false;
+            }
+            return true;
+        }
+
+       
+    }
+}
--- trunk/iaai/iaai/profesor/AltaProfesor.Designer.cs
+++ trunk/iaai/iaai/profesor/AltaProfesor.Designer.cs
@@ -28,34 +28,199 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.email = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.direccion = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.fecha_nacimiento = new System.Windows.Forms.MaskedTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.apellido = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dni = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.telefono_carac = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.telefono_numero = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.nombre = new System.Windows.Forms.TextBox();
+            this.guardar = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(325, 220);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(88, 46);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // email
             // 
-            this.email.Location = new System.Drawing.Point(65, 25);
+            this.email.Location = new System.Drawing.Point(396, 66);
             this.email.Name = "email";
             this.email.Size = new System.Drawing.Size(100, 20);
             this.email.TabIndex = 1;
+            this.email.Leave += new System.EventHandler(this.validar_mail);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(356, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "e-mail";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(338, 41);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 13);
+            this.label9.TabIndex = 35;
+            this.label9.Text = "Dirección";
+            // 
+            // direccion
+            // 
+            this.direccion.Location = new System.Drawing.Point(396, 38);
+            this.direccion.Name = "direccion";
+            this.direccion.Size = new System.Drawing.Size(137, 20);
+            this.direccion.TabIndex = 27;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(443, 15);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(10, 13);
+            this.label7.TabIndex = 33;
+            this.label7.Text = "-";
+            // 
+            // fecha_nacimiento
+            // 
+            this.fecha_nacimiento.Location = new System.Drawing.Point(137, 89);
+            this.fecha_nacimiento.Mask = "00/00/0000";
+            this.fecha_nacimiento.Name = "fecha_nacimiento";
+            this.fecha_nacimiento.Size = new System.Drawing.Size(68, 20);
+            this.fecha_nacimiento.TabIndex = 23;
+            this.fecha_nacimiento.ValidatingType = typeof(System.DateTime);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(87, 41);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 32;
+            this.label6.Text = "Apellido";
+            // 
+            // apellido
+            // 
+            this.apellido.Location = new System.Drawing.Point(137, 38);
+            this.apellido.Name = "apellido";
+            this.apellido.Size = new System.Drawing.Size(137, 20);
+            this.apellido.TabIndex = 19;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(105, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "DNI";
+            // 
+            // dni
+            // 
+            this.dni.Location = new System.Drawing.Point(137, 63);
+            this.dni.Name = "dni";
+            this.dni.Size = new System.Drawing.Size(137, 20);
+            this.dni.TabIndex = 21;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 13);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Fecha de Nacimiento";
+            // 
+            // telefono_carac
+            // 
+            this.telefono_carac.Location = new System.Drawing.Point(396, 12);
+            this.telefono_carac.Name = "telefono_carac";
+            this.telefono_carac.Size = new System.Drawing.Size(41, 20);
+            this.telefono_carac.TabIndex = 24;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(341, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Teléfono";
+            // 
+            // telefono_numero
+            // 
+            this.telefono_numero.Location = new System.Drawing.Point(459, 12);
+            this.telefono_numero.Name = "telefono_numero";
+            this.telefono_numero.Size = new System.Drawing.Size(74, 20);
+            this.telefono_numero.TabIndex = 25;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(87, 15);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Nombre";
+            // 
+            // nombre
+            // 
+            this.nombre.Location = new System.Drawing.Point(137, 12);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(137, 20);
+            this.nombre.TabIndex = 18;
+            // 
+            // guardar
+            // 
+            this.guardar.Location = new System.Drawing.Point(146, 158);
+            this.guardar.Name = "guardar";
+            this.guardar.Size = new System.Drawing.Size(75, 23);
+            this.guardar.TabIndex = 36;
+            this.guardar.Text = "Aceptar";
+            this.guardar.UseVisualStyleBackColor = true;
+            this.guardar.Click += new System.EventHandler(this.guardar_Click);
+            // 
+            // Cancel
+            // 
+            this.Cancel.Location = new System.Drawing.Point(362, 158);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(75, 23);
+            this.Cancel.TabIndex = 37;
+            this.Cancel.Text = "Cancelar";
+            this.Cancel.UseVisualStyleBackColor = true;
             // 
             // Altaprofesor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(460, 326);
+            this.ClientSize = new System.Drawing.Size(563, 204);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.guardar);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.direccion);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.fecha_nacimiento);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.apellido);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dni);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.telefono_carac);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.telefono_numero);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.nombre);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.email);
-            this.Controls.Add(this.button1);
             this.Name = "Altaprofesor";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -65,7 +230,23 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox direccion;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.MaskedTextBox fecha_nacimiento;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox apellido;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox dni;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox telefono_carac;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox telefono_numero;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox nombre;
+        private System.Windows.Forms.Button guardar;
+        private System.Windows.Forms.Button Cancel;
     }
 }
