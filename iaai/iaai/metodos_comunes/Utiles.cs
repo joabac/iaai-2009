8020528750c4beb7ef495091af1755d165c2c807
joabac <joabac@f1b130d6-d504-11de-bff8-d51c198f79d0>
2009-11-29 14:15:16 +0000

    corri el metodo validar email

--- iaai/iaai/metodos_comunes/Utiles.cs
+++ iaai/iaai/metodos_comunes/Utiles.cs
@@ -44,6 +44,156 @@ namespace iaai.metodos_comunes
             return true;
         }
 
+        /// <summary>
+        /// Valida estructura de correo electronico
+        /// </summary>
+        /// <param name="email">String con email a validar</param>
+        /// <returns>true o false en funcion del resultado obtenido</returns>
+        private bool validar_email(string email)
+        {
+            if (email = "")
+            {
+                bool estado_email = true;
+
+                int i = 0;
+
+                if (email.Length != 0)
+                {
+                    if (email[i] == '@' || email[i] == '_' || email[i] == '-' || email[i] == '.')
+                    {
+
+                        estado_email = false;
+
+                    }
+                    else
+                    {
+                        for (int j = 0; j < email.Length - 1; j++)//verifico por caracteres especiales junto a un punto
+                        {
+
+                            if (((email[j] == '.' || email[j] == '_' || email[j] == '-') && (email[j + 1] == '.' || email[j + 1] == '@')))
+                                estado_email = false;
+                        }
+
+                        if (estado_email == true)
+                        {
+
+                            while (email[i] != '@' && i < email.Length - 1)//verifico primera parte aaaa@
+                            {
+                                if (Char.IsDigit(email[i]) || Char.IsLetter(email[i]) ||
+                                    email[i] == '_' || email[i] == '-' || email[i] == '.')
+                                {
+                                }
+                                else
+                                {
+
+
+                                    estado_email = false;
+
+                                }
+                                i++;
+                            }
+
+                            if (email[i] == '@' && estado_email == true && email.Length - 1 > i)
+                            { //verifico luego del arroba  @aaaa.
+
+                                i++;
+
+                                if (email[i] == '_' || email[i] == '-' || email[i] == '.')
+                                { //primer caracter luego de arroba
+
+                                    estado_email = false;
+
+                                }
+                                else
+                                { //verifico sean o digitos o letras o los caracteres permitidos
+
+                                    //desde el caracter 1 luego del arroba asta el primer punto
+                                    while (email[i] != '.' && i < email.Length - 1)//verifico primera parte aaaa@
+                                    {
+                                        if (Char.IsDigit(email[i]) || Char.IsLetter(email[i]) ||
+                                            email[i] == '_' || email[i] == '-' || email[i] == '.')
+                                        {
+                                        }
+                                        else
+                                        {
+
+                                            estado_email = false;
+
+                                        }
+                                        i++;
+                                    }
+
+                                    if (email[i] == '.' && estado_email == true && email.Length - 1 > i)
+                                    { //todo lo demas
+
+                                        i++;
+
+                                        while (i < email.Length - 1)//verifico la ultima parte
+                                        {
+
+                                            if (email[i] == '_' || email[i] == '-')
+                                            {
+
+                                                estado_email = false;
+
+                                            }
+
+                                            else
+                                            {
+                                                if (Char.IsDigit(email[i]) || Char.IsLetter(email[i]) || email[i] == '.')
+                                                {
+                                                }
+                                                else
+                                                {
+
+                                                    estado_email = false;
+
+                                                }
+                                            }
+                                            i++;
+                                        }
+                                        if (!Char.IsLetterOrDigit(email[i]))
+                                        {
+
+                                            estado_email = false;
+
+                                        }
+                                    }
+                                    else
+                                    {
+
+                                        estado_email = false;
+
+                                    }
+
+                                }
+
+                            }
+                            else
+                            {
+
+                                estado_email = false;
+
+                            }
+                        }
+                        else
+                        {
+
+                            estado_email = false;
+                        }
+
+                    }
+                }
+                else
+                {
+
+                    estado_email = false;
+
+                }
+                return estado_email;
+            }
+        }
+
         struct prueba
         {
             string cadena;
