5434300549a30fe1292050f484b14c0468210c7f
joabac <joabac@f1b130d6-d504-11de-bff8-d51c198f79d0>
2009-12-01 12:59:47 +0000

    cambio para restaurar proyecto

---
+++ trunk/iaai/iaai/profesor/Eliminar_Profesor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using iaai.metodos_comunes;
+
+namespace iaai.profesor
+{
+    public partial class Eliminar_Profesor : Form
+    {
+        Profesor profesor_encontrado = new Profesor();
+        Data_base.Data_base db = new iaai.Data_base.Data_base();
+        Utiles metodo = new Utiles();
+
+        public Eliminar_Profesor()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBuscar_Click(object sender, EventArgs e)
+        {
+
+            if (metodo.ValidarDni(textBoxDni.Text))
+            {
+                profesor_encontrado = db.Buscar_Profesor(textBoxDni.Text);
+
+                if (profesor_encontrado == null)
+
+                    MessageBox.Show("El DNI no es de un profesor del Instituto");
+                else
+                {
+                    db.eliminarProfesor(profesor_encontrado.getDni());
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("El DNI ingresado es incorrecto");
+            }
+
+        }
+
+        private void buttonAceptar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
