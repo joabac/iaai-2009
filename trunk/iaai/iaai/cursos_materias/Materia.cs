e3c9c128c0b9a7b66f2602f434a526d85856fe2f
joabac <joabac@f1b130d6-d504-11de-bff8-d51c198f79d0>
2009-12-03 23:51:41 +0000

    nuva clase

--- trunk/iaai/iaai/cursos_materias/Materia.cs
+++ trunk/iaai/iaai/cursos_materias/Materia.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using iaai.profesor;
 
 namespace iaai.cursos_materias
 {
@@ -15,17 +16,47 @@ namespace iaai.cursos_materias
         public int nivel {get; set;}
         public string nombre{get; set;}
         public int id_profesor{get; set;}
-        public int cupo{get; set;}
-        public int turno{get; set;}
-
-        public Materia(){}
-
-        public Materia(int id_materia){
         
         
+
+        Data_base.Data_base db = new iaai.Data_base.Data_base();
+       
+        public List<Turno> turno_materia { get; set; }
+
+        public Materia(){
+ 
         }
 
-        
+        public Materia(int id_mat, int id_profe){
 
+            turno_materia = db.getTurno(id_mat);
+           
+        }
+
+        public void cargar()
+        {
+            turno_materia = db.getTurno(id_materia);
+            
+        }
+
+        public string get_adjunto(string turno) {
+
+            foreach (Turno turno_tem in turno_materia)
+            {
+                if (turno_tem.turno.Contains(turno))
+                    return (turno_tem.adjunto.getApellido()+", "+turno_tem.adjunto.getNombre());
+            }
+            return "";
+        }
+
+        internal int get_id_turno(string turno)
+        {
+            foreach (Turno turno_tem in turno_materia)
+            {
+                if (turno_tem.turno.Contains(turno))
+                    return turno_tem.id_turno;
+            }
+            return -1;
+        }
     }
 }
