bac40d33e4ff952b7a2a98a2f9e7dd3e2d8df1fb
joabac <joabac@f1b130d6-d504-11de-bff8-d51c198f79d0>
2009-12-03 23:50:55 +0000

    clase turnos

---
+++ trunk/iaai/iaai/cursos_materias/Turno.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using iaai.profesor;
+
+namespace iaai.cursos_materias
+{
+ 
+    class Turno
+    {
+        public int id_turno { get; set; }
+        public int id_profesor { get; set; }
+        public string turno { get; set; }
+        public int cupo { get; set; }
+        public int materia { get; set; }
+        public Profesor adjunto { get; set; }
+
+        Data_base.Data_base db = new iaai.Data_base.Data_base();
+
+        public Turno() {
+
+       
+        }
+
+        public void cargar() 
+        {
+            adjunto = db.Buscar_Profesor(id_profesor);
+        }
+
+       
+    }
+
+}
