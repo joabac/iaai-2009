4d90f9812588a9203403d308202c50dd442246d5
joabac <joabac@f1b130d6-d504-11de-bff8-d51c198f79d0>
2009-12-06 21:57:40 +0000

    Clase Curso

---
+++ iaai/iaai/cursos_materias/Curso.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iaai.cursos_materias
+{
+    class Curso
+    {
+        public int id_curso { get; set; }
+        public int nivel { get; set; }
+        public string nombre { get; set; }
+        public int id_profesor { get; set; }
+        public int cupo { get; set; }
+        public string condicion { get; set; }
+        public int id_area { get; set; }
+        public int duracion { get; set; }
+
+        public Curso()
+        {
+
+        }
+
+
+    }
+}
