a72d43163434559a9baf49981250cbfb7bc1fd02
joabac <joabac@f1b130d6-d504-11de-bff8-d51c198f79d0>
2009-12-06 21:57:57 +0000

    Clase Inscripto Curso

---
+++ trunk/iaai/iaai/cursos_materias/Inscripto_curso.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iaai.cursos_materias
+{
+    class Inscripto_curso
+    {
+        public Curso curso_inscripto { get; set; }
+
+        public string condicion { get; set; } //condicion en que quedo inscripto
+
+        public int id_inscripcion_curso { get; set; } //registro de transaccion
+
+        
+
+        public Inscripto_curso() 
+        { 
+        }
+    }
+}
