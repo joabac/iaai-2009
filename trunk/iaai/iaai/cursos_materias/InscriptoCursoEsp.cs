f66399018f8e680d46b506396f639b5749593df1
joabac <joabac@f1b130d6-d504-11de-bff8-d51c198f79d0>
2009-12-07 23:37:15 +0000

    nueva clase para manejo de cursos especiales

---
+++ trunk/iaai/iaai/cursos_materias/InscriptoCursoEsp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iaai.cursos_materias
+{
+    class InscriptoCursoEsp
+    {
+
+
+        public CursosEsp curso_inscripto { get; set; }
+
+        public string condicion { get; set; } //condicion en que quedo inscripto
+
+        public int id_inscripcion_curso { get; set; } //registro de transaccion
+
+        
+
+        public InscriptoCursoEsp() 
+        { 
+        }
+    }
+}
